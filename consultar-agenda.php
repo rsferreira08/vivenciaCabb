fdd834f4c2152ac001eba7c301ce7eaea78dc110
zoga08 <41244597+zoga08@users.noreply.github.com>
2018-10-20 07:40:11 -0300

    card description update

--- consultar-agenda.php
+++ consultar-agenda.php
@@ -18,40 +18,44 @@ if (!$isValid) {
 $turmaArray = Agenda::pesquisaPorData(strtotime($dataHora));
 ?>
 
-<div class="event-info">
-	<h1 style="padding: 20px;">Inscritos</h1>
-	<div class="row">
-		<div class="col-lg-5">
-			<?php 
-			if ( sizeof($turmaArray) > 3 ) {
-				for ( $i=0; $i<=2; $i++ ) { ?>
-
-					<p>Nome: <?=$turmaArray[$i]['nome']?></p>
-					<p>Matricula: <?=$turmaArray[$i]['matricula']?></p>
-					<hr>
-
-			<?php 
-				}
-			} else {
-				for ( $i=0; $i<sizeof($turmaArray); $i++ ) { ?>
-
-					<p>Nome: <?=$turmaArray[$i]['nome']?></p>
-					<p>Matricula: <?=$turmaArray[$i]['matricula']?></p>
-					<hr>
-			<?php } } ?>
-		</div>
-		<div class="col-lg-5">
-			<?php 
-			if ( sizeof($turmaArray) > 3 ) {
-				for ( $i=3; $i<sizeof($turmaArray); $i++ ) { ?>
-
-					<p>Nome: <?=$turmaArray[$i]['nome']?></p>
-					<p>Matricula: <?=$turmaArray[$i]['matricula']?></p>
-					<hr>
-				
-			<?php } } ?>
-		</div>
-	</div>
+<div class="event-info""> 
+    <h1 style="padding: 20px;">Inscritos</h1> 
+    <div class="row"> 
+        <div class="col-lg-6"> 
+            <?php  
+            if ( sizeof($turmaArray) > 3 ) { 
+                for ( $i=0; $i<=2; $i++ ) { ?> 
+                    <div class="mr-3" style="float: left"> 
+                        <img alt="<?=strtoupper($turmaArray[$i]['matricula'])?>" src="https://humanograma.intranet.bb.com.br/avatar/<?=strtoupper($turmaArray[$i]['matricula'])?>" class="rounded-circle mt-2" width="50px;"> 
+                    </div> 
+                    <p>Nome: <?=$turmaArray[$i]['nome']?></p> 
+                    <p>Matricula: <?=$turmaArray[$i]['matricula']?></p> 
+                    <hr> 
+ 
+            <?php  
+                } 
+            } else { 
+                for ( $i=0; $i<sizeof($turmaArray); $i++ ) { ?> 
+                    <div class="mr-3" style="float: left"> 
+                        <img alt="<?=strtoupper($turmaArray[$i]['matricula'])?>" src="https://humanograma.intranet.bb.com.br/avatar/<?=strtoupper($turmaArray[$i]['matricula'])?>" class="rounded-circle mt-2" width="50px;"> 
+                    </div> 
+                    <p>Nome: <?=$turmaArray[$i]['nome']?></p> 
+                    <p>Matricula: <?=$turmaArray[$i]['matricula']?></p> 
+                    <hr> 
+            <?php } } ?> 
+        </div> 
+        <div class="col-lg-6"> 
+            <?php  
+            if ( sizeof($turmaArray) > 3 ) { 
+                for ( $i=3; $i<sizeof($turmaArray); $i++ ) { ?> 
+                    <div class="mr-3" style="float: left"> 
+                        <img alt="<?=strtoupper($turmaArray[$i]['matricula'])?>" src="https://humanograma.intranet.bb.com.br/avatar/<?=strtoupper($turmaArray[$i]['matricula'])?>" class="rounded-circle mt-2" width="50px;"> 
+                    </div> 
+                    <p>Nome: <?=$turmaArray[$i]['nome']?></p> 
+                    <p>Matricula: <?=$turmaArray[$i]['matricula']?></p> 
+                    <hr> 
+                 
+            <?php } } ?> 
+        </div> 
+    </div> 
 </div>
-
-<?php ?>
