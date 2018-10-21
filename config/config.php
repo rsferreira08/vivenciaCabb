4cd07347803b904017f86d34fb46eea9fbb27ec7
Rafael Ferreira <rsferreira11@gmail.com>
2018-10-21 22:40:01 +0300

    rollback to docker config

--- config/config.php
+++ config/config.php
@@ -3,13 +3,13 @@
 date_default_timezone_set('America/Sao_Paulo');
 
 // get document root
-define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT'].'/vivenciaCabb');
+define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT'].'/');
 
 // set this the base url of the project.
-define('BASE_URL', 'http://localhost/vivenciaCabb');
+define('BASE_URL', 'http://localhost/');
 
 // define Database Variables
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mariadb');
 define('DB_NAME', 'db_vivencia');
 define('DB_USER', 'root');
 define('DB_PASS', '');
